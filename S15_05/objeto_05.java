a46de26196e52375daa44e9fb0e18afdde883b9d
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2023-11-07 20:43:33 -0500

    Primer ejercicio

---
+++ S15_05/objeto_05.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package S15_05;
+
+/**
+ *
+ * @author Usuario3
+ */
+public class objeto_05 {
+    /*Atributos*/
+    String nombre;
+    String fabracion;
+    String material;
+    int costo;
+    
+    /*Metodos*/
+    public void fabricacion(){
+        System.out.println("Se fabrico el año 2020");
+    }
+    public void costo(){
+        System.out.println("Su costo fue de s/90.00");
+    }
+    public void material(){
+        System.out.println("Esta echo de madera");
+    }
+    public void uso(){
+        System.out.println("Se usa de asiento a las personas");
+    }
+}
